e331134dbba936a738a0146b64289bab31234cec
LISA <elizaveta_tatarinova@epam.com>
2022-02-07 18:06:43 +0300

    test suites added (all 3), pom-file updated

--- src/test/java/com/epam/tc/hw1/DataProviderForCalculator.java
+++ src/test/java/com/epam/tc/hw1/DataProviderForCalculator.java
@@ -17,8 +17,8 @@ public class DataProviderForCalculator {
     @DataProvider
     public static Object[][] getTestDataForAdditionNegative(){
         return new Object[][]{
-                {23137.23, 123.12, 5},
-                {1.7976931348623158e+308 + 1, 1.7976931348623158e+308 + 1, 1.7976931348623158e+308*2 + 2}
+                {23137.23, 123.12, 5}
+                //{1.7976931348623158e+308 + 1, 1.7976931348623158e+308 + 1, }
         };
     }
 
