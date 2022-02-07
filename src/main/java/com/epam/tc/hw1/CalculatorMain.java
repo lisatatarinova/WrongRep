8ae78ed854fdbf399a4bbf7ec83c83b0f480744d
LISA <elizaveta_tatarinova@epam.com>
2022-02-07 18:27:51 +0300

    a little change to make a pull request creation available

--- src/main/java/com/epam/tc/hw1/CalculatorMain.java
+++ src/main/java/com/epam/tc/hw1/CalculatorMain.java
@@ -5,6 +5,8 @@ import com.epam.tat.module4.Calculator;
 public class CalculatorMain {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+
+        //a little change to make a pull request creation available
     }
 
 }
